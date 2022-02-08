a1a306890d040677f246a0ced7ba2fa8afb9db96
Soufiane <soufianetai@hotmail.it>
2022-02-08 15:07:23 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar() {
+    return (
+    <nav class = 'navbar bg-light'>
+       <a class = 'navbar' href='http:localhost:3000/To-do-list'>Home</a>
+    </nav>);
+}
+
+export default Navbar;
